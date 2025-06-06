35678f34a64022e013017a7ee09840b9b00f9648
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-06-06 13:28:14 +0530

    added non bdd style algorithm

---
+++ src/test/java/ex_04_RestAssured_HTTP_Methods/APITesting008_GET_BDDStyle.java
@@ -0,0 +1,24 @@
+package ex_04_RestAssured_HTTP_Methods;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
+public class APITesting008_GET_BDDStyle {
+
+    @Test
+    public void test_GET_Positive(){
+        String pin_code = "388620";
+
+                given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pin_code)
+                .when()
+                .log()
+                .all()
+                .get()
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+}
